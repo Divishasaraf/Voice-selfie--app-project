cf43260b76eb7cc780d9314adb970002ecc57701
Divi <82699192+Divishasaraf@users.noreply.github.com>
2022-02-22 18:36:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var SpeechRecgnition = window.webkitSpeechRecognition;
+
+var recognition  = new SpeechRecgnition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event) 
+{
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    console.log(Content);
+    
+    document.getElementById("textbox").innerHTML = Content;
+}
